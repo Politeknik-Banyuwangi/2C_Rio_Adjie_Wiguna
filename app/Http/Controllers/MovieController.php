914475ec9550ba295e87f9bbc3b10585734b345f
Rio Adjie Wiguna <92602362+rioadjie@users.noreply.github.com>
2022-11-25 02:40:26 +0700

    Update MovieController.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -70,7 +70,7 @@ class MovieController extends Controller
         ->get('https://api.themoviedb.org/3/movie/' . $id . '/recommendations')
         ->json()['results'];
 
-        dump($movie);
+//         dump($movie);
         return view('movie.show', [
             'movie' => $movie,
             'movieRecommendations' => $movieRecommendations
